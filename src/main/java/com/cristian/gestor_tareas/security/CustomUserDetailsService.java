baf39b44cbd34aaf979b58fe68ad88c60f1e0365
Cristian Alhambra <cristianalh@gmail.com>
2026-03-23 20:34:23 +0100

    Para que el usuario se pueda autentificar y añadir/borrar tareas

---
+++ src/main/java/com/cristian/gestor_tareas/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.cristian.gestor_tareas.security;
+
+
+import com.cristian.gestor_tareas.model.User;
+import com.cristian.gestor_tareas.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.get().getUsername())
+                .password(user.get().getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
