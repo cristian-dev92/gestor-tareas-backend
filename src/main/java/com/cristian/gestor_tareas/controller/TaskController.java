8e26cb5a0b0534a74ba2ec7541ff3738b605c1e7
Cristian Alhambra <cristianalh@gmail.com>
2026-03-31 12:51:29 +0200

    Modificamos el archivo para añadir las subtareas

--- src/main/java/com/cristian/gestor_tareas/controller/TaskController.java
+++ src/main/java/com/cristian/gestor_tareas/controller/TaskController.java
@@ -1,7 +1,10 @@
 package com.cristian.gestor_tareas.controller;
 
+import com.cristian.gestor_tareas.model.Subtask;
 import com.cristian.gestor_tareas.model.Task;
 import com.cristian.gestor_tareas.model.User;
+import com.cristian.gestor_tareas.repository.SubtaskRepository;
+import com.cristian.gestor_tareas.repository.TaskRepository;
 import com.cristian.gestor_tareas.service.TaskService;
 import com.cristian.gestor_tareas.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,10 +19,18 @@ public class TaskController {
 
     private final TaskService taskService;
     private final UserService userService;
+    private final TaskRepository taskRepository;
+    private final SubtaskRepository subtaskRepository;
 
-    public TaskController(TaskService taskService, UserService userService) {
+    public TaskController(TaskService taskService,
+                          UserService userService,
+                          TaskRepository taskRepository,
+                          SubtaskRepository subtaskRepository)
+    {
         this.taskService = taskService;
         this.userService = userService;
+        this.taskRepository = taskRepository;
+        this.subtaskRepository = subtaskRepository;
     }
 
 
@@ -86,5 +97,27 @@ public class TaskController {
         }
 
         return ResponseEntity.ok(taskService.updateTask(task));
+    }
+
+    //Crear subtarea
+    @PostMapping("/{taskId}/subtasks")
+    public Subtask addSubtask(@PathVariable Long taskId, @RequestBody Subtask subtask) {
+        Task task = taskRepository.findById(taskId).orElseThrow();
+        subtask.setTask(task);
+        return subtaskRepository.save(subtask);
+    }
+
+    //Cambiar estado
+    @PatchMapping("/subtasks/{id}/toggle")
+    public Subtask toggleSubtask(@PathVariable Long id) {
+        Subtask s = subtaskRepository.findById(id).orElseThrow();
+        s.setDone(!s.isDone());
+        return subtaskRepository.save(s);
+    }
+
+    //Borrar subtarea
+    @DeleteMapping("/subtasks/{id}")
+    public void deleteSubtask(@PathVariable Long id) {
+        subtaskRepository.deleteById(id);
     }
 }
