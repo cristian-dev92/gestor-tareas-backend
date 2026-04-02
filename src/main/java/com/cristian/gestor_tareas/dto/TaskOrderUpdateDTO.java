c6f8110087aab54c4b9062a6c95f92564a657ce4
Cristian Alhambra <cristianalh@gmail.com>
2026-04-02 16:51:25 +0200

    Añadimos el Dto para la orden de las tareas

---
+++ src/main/java/com/cristian/gestor_tareas/dto/TaskOrderUpdateDTO.java
@@ -0,0 +1,12 @@
+package com.cristian.gestor_tareas.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskOrderUpdateDTO {
+    private Long id;
+    private Integer orderIndex;
+
+}
